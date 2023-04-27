95dbc4626f7c7ee5c5eb9ae128cf110148787872
Juan Manuel Quinteros <juanmanuelquinteros3@gmail.com>
2023-04-27 15:53:09 -0300

    first commit

---
+++ src/main/java/guru/springframework/chucknorrisjokes/services/JokeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.chucknorrisjokes.services;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public interface JokeService {
+
+    String getJoke();
+
+
+}
