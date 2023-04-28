986762d23933d43d9d8ade65888afc7eac3c7bf3
Juan Manuel Quinteros <juanmanuelquinteros3@gmail.com>
2023-04-28 12:37:29 -0300

    Add Controller and Template

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,13 +1,13 @@
-package guru.springframework.chucknorrisjokes;
+package guru.springframework.spring5jokesappv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChucknorrisjokesApplication {
+public class Spring5JokesAppV2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChucknorrisjokesApplication.class, args);
+		SpringApplication.run(Spring5JokesAppV2Application.class, args);
 	}
 
 }
--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -1,4 +1,4 @@
-package guru.springframework.chucknorrisjokes.services;
+package guru.springframework.spring5jokesappv2.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
